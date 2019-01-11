d6dba42c9092c272cec14cc04687f68f7d127a08
zhoujiahua <jiahuasir@gmail.com>
2019-01-11 17:31:14 +0800

    配置跨域访问

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require("path");
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
@@ -11,21 +11,31 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 
+//设置跨域访问
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 // load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
 
 // Passport Config
-require('./config/passport')(passport);  
+require('./config/passport')(passport);
 
 // Connect to mongoose
 mongoose.connect("mongodb://192.168.1.168/cnkiapp")
-        .then(() => {
-          console.log("MongoDB connected....");
-        })
-        .catch(err => {
-          console.log(err);
-        })
+  .then(() => {
+    console.log("MongoDB 数据库连接成功！");
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 // 引入模型
 require("./models/Idea");
@@ -40,10 +50,12 @@ app.set('view engine', 'handlebars');
 
 // body-parser middleware
 var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
 
 // 使用静态文件
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // method-override middleware
 app.use(methodOverride('_method'));
@@ -62,7 +74,7 @@ app.use(passport.session());
 app.use(flash());
 
 // 配置全局变量
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.user = req.user || null;
@@ -70,24 +82,23 @@ app.use((req,res,next) => {
 })
 
 // 配置路由
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
   const title = "陕西分公司项目建设";
-  res.render("index",{
-    title:title
+  res.render("index", {
+    title: title
   });
 })
 
-app.get("/about",(req,res) => {
+app.get("/about", (req, res) => {
   res.render("about");
 })
 
-
 // 使用routes
-app.use("/ideas",ideas);
-app.use("/users",users);
+app.use("/ideas", ideas);
+app.use("/users", users);
 
+//服务端口监听
 const port = 5000;
-
-app.listen(port,() => {
-  console.log(`Server started on ${port}`);
+app.listen(port, () => {
+  console.log(`正在监听：${port} 端口`);
 })
