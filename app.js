dc82056a0b5fa63af90683527360346dcce52b6b
zhoujiahua <jiahuasir@gmail.com>
2019-01-11 17:53:24 +0800

    去除跨域访问

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const bcrypt = require('bcrypt');
 const app = express();
 
 //设置跨域访问
-app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", " 3.2.1");
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+// app.all("*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("X-Powered-By", " 3.2.1");
+//   res.header("Content-Type", "application/json;charset=utf-8");
+//   next();
+// });
 
 // load routes
 const ideas = require('./routes/ideas');
@@ -46,6 +46,7 @@ const Idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
+
 app.set('view engine', 'handlebars');
 
 // body-parser middleware
